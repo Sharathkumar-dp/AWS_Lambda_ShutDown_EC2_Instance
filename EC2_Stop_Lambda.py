9323d64b8eb8ec912aa2d421221e1c3afee34d89
Sharathkumar <71508900+Sharathkumar-dp@users.noreply.github.com>
2022-03-29 15:24:59 +0530

    Update EC2_Stop_Lambda.py

--- EC2_Stop_Lambda.py
+++ EC2_Stop_Lambda.py
@@ -1,19 +1,23 @@
 import boto3
 def lambda_handler(event, context):
-
-	#To get list of regions
-	ec2_client = boto3.client('ec2')
-	regions = [region['RegionName']
-		for region in ec2_client.describe_regions()['Regions']]
-
+	
+	#to get list of regions
+	ec2_client = bto3.client('ec2')
+	regions = [region['regionName']
+				for region in ec2_client.describe_regions()['Regions']]
+				
+				
 	#To iterate over each regions
 	for region in regions:
 		ec2 = boto3.resource('ec2', region_name=region)
-		print("region : ", region)
-		#To get only instances in running state
-		instances - ec2.instances.filter(
-			Filters=[{'Name': 'instance-state-name','Values':[+'running']}])
-		#To to stopr running instances
+		print("Region : ", region)
+		
+		#To get list of running instances
+		instances = ec2.instances.filter(
+			Filters=[{'Name': 'instance-state-name',
+					  'Values': ['running']}])
+					  
+		#To stop Running instances
 		for instance in instances:
 			instance.stop()
-					print('Stopped instance : ',instance.id)
+			print('Stopped instance : ',instance.id)
